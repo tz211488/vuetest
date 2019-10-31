4da0d2bf61e11779daf9c642985ceba5629298ff
wo1211 <myaiweisite@gmail.com>
2019-11-01 00:15:01 +0800

    设置

---
+++ src/components/header/headerTop.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="header-top">
+    <span>Free Shipping Over the World, New User 10% OFF (Use Code: BZEEYNEW)</span>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "headerTop"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/header/headerWrapper.vue
@@ -0,0 +1,31 @@
+<template>
+<header>
+    <header-top />
+    <header-panel />
+    <header-nav />
+</header>
+
+</template>
+
+<script>
+    import headerTop from "./headerTop";
+    import headerNav from "./headerNav";
+    import headerPanel from "./headerPanel";
+    export default {
+        name: "headerWrapper",
+        components:{
+            headerTop,
+            headerPanel,
+            headerNav,
+        }
+    }
+</script>
+
+<style scoped>
+    .header-top{
+        font-size: 1rem;
+        line-height: 3rem;
+        text-align: center;
+    }
+
+</style>
